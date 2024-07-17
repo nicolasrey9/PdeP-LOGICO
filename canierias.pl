a209fbb927ed2f993d8477dde7c891fb8933b8b5
NicolasRey <nicolasreypc@gmail.com>
2024-07-16 21:14:40 -0300

    .

--- canierias.pl
+++ canierias.pl
@@ -14,17 +14,28 @@ precioPieza(canilla(_, _, Ancho), 15) :-
 
 
 %%%%%%%%%%% PUNTO 2
-puedoEnchufar(Pieza1, Pieza2) :-
-    color(Pieza1, Color),
-    color(Pieza2, Color).
-
-puedoEnchufar(Pieza1, Pieza2) :-
-    color(Pieza1, Color1),
-    color(Pieza2, Color2),
-    sonEnchufables(Color1, Color2).
-
 color(codo(Color), Color).
 color(canio(Color, _), Color).
 color(canilla(_, Color, _), Color).
 
-sonEnchufables(Color1, Color2) :-
+coloresEnchufables(azul, rojo).
+coloresEnchufables(rojo, negro).
+
+
+colorDerecho(Canieria, ColorDerecho) :-
+    reverse(CanieriaInvertida, Canieria),
+    nth0(0, CanieriaInvertida, PiezaDerecha),
+    color(PiezaDerecha, ColorDerecho).
+
+colorIzquierdo(Canieria, ColorIzquierdo) :-
+    nth0(0, Canieria, PiezaIzquierda),
+    color(PiezaIzquierda, ColorIzquierdo).
+
+
+puedoEnchufar(Canieria, Canieria) :-
+    colorDerecho(Canieria, Color),
+    colorIzquierdo(Canieria, Color).
+puedoEnchufar(Canieria, Canieria) :-
+    colorDerecho(Canieria, Color1),
+    colorIzquierdo(Canieria, Color2),
+    coloresEnchufables(Color1, Color2).
---
+++ carcel.pl
@@ -0,0 +1,62 @@
+ https://docs.google.com/document/d/12zUNFV4K7Iofc47FN-b7O-gXjTrqgP4dQh47yJVlvw0/edit
+
+% guardia(Nombre)
+guardia(bennett).
+guardia(mendez).
+guardia(george).
+
+% prisionero(Nombre, Crimen)
+prisionero(piper, narcotrafico([metanfetaminas])).
+prisionero(alex, narcotrafico([heroina])).
+prisionero(alex, homicidio(george)).
+prisionero(red, homicidio(rusoMafioso)).
+prisionero(suzanne, robo(450000)).
+prisionero(suzanne, robo(250000)).
+prisionero(suzanne, robo(2500)).
+prisionero(dayanara, narcotrafico([heroina, opio])).
+prisionero(dayanara, narcotrafico([metanfetaminas])).
+
+
+% controla(Controlador, Controlado)
+controla(piper, alex).
+controla(bennett, dayanara).
+controla(Guardia, Otro):- prisionero(Otro,_), guardia(Guardia), not(controla(Otro, Guardia)).
+
+
+conflictoDeIntereses(Persona1, Persona2) :-
+    controla(Persona1, X),
+    controla(Persona2, X),
+    not(controla(Persona1, Persona2)),
+    not(controla(Persona2, Persona1)),
+    Persona1 \= Persona2.
+
+
+peligroso(Preso) :-
+    prisionero(Preso, _),
+    forall(prisionero(Preso, Crimenes), crimenGrave(Crimenes)).
+
+crimenGrave(homicidio(_)).
+crimenGrave(narcotráfico(Drogas)):-
+    length(Drogas, Cantidad),
+    Cantidad >= 5.
+crimenGrave(narcotráfico(Drogas)):-
+    member(metanfetaminas, Drogas).
+    
+    
+ladronDeGuanteBlanco(Ladron):-
+    prisionero(Ladron, _),
+    forall(prisionero(Ladron, Crimenes), roboCaro(Crimenes)).
+
+roboCaro(robo(Monto)) :- Monto > 100000.
+
+
+condena(Prisionero, Condena):-
+    prisionero(Prisionero, _),
+    findall(Pena, (prisionero(Prisionero, Crimen), pena(Crimen, Pena)), PenaPorCrimen),
+    sumlist(PenaPorCrimen, Condena).
+
+pena(robo(Monto), Pena) :- Pena is Monto / 10000.
+pena(homicidio(Muerto), 7) :- guardia(Muerto).
+pena(homicidio(Muerto), 5) :- not(guardia(Muerto)).
+pena(narcotrafico(Drogas), Pena) :- length(Drogas, Cantidad), Pena is Cantidad * 2.
+
